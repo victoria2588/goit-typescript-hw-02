7712351a7562750547095352a45f8981a877247f
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-10-05 14:26:32 +0300

    commit 003
    fixed

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,7 +3,7 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T>(): Promise<[string, number]> {
+function getPromise(): Promise<[string, number]> {
   return new Promise((resolve) => {
     resolve(["Text", 50]);
   });
